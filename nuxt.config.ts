177885599f6d3fb48b8b9d9d5121c697b80cc15b
inoshiro <inoshirou@gmail.com>
2024-08-14 18:19:45 +0900

    gaのテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,26 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-    app: {
+  app: {
     head: {
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ""},
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@100..700&display=swap' }
-      ]
-    }
-  }
-})
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@100..700&display=swap",
+        },
+      ],
+    },
+  },
+
+  modules: ["nuxt-gtag"],
+
+  gtag: {
+    id: "G-3Z9G0K8Q44",
+  },
+});
